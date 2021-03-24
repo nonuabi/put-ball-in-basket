6e73740809e225ca1823c1149bc97ecb81229a32
nonuabi <nonuabi@gmail.com>
2021-03-24 14:26:37 +0530

    by click button balls are adding into the container

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-$(".ball").hide();
+
+var arr = [
+  "red",
+  "blue",
+  "yellow",
+  "lightgrey",
+  "darkorchid",
+  "black",
+  "orange",
+  "deeppink",
+  "green",
+  "purple",
+  "saddlebrown",
+  "lightseagreen",
+  "deepskyblue",
+  "firebrick",
+  "crimson",
+];
+
+function random() {
+    return Math.floor(Math.random() * arr.length);
+}
+
+
 $("button").on("click", function () {
   $(".container").append("<div class='ball'></div>");
 });
+
+
